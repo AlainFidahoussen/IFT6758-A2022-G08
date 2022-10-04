2465e68cc84c6155e696628cc483840d38e44162
Alain Fidahoussen <alain.fidahoussen@umontreal.ca>
2022-10-04 09:19:17 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='NHL - IFT6758 - Fall 2022 - Group 08',
+    author='G08',
+    license='',
+)
