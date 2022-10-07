60eefd4a44d31ab2e316fb73e9e67c9043768e3e
Alain Fidahoussen <alain.fidahoussen@ultra-ft.com>
2022-10-07 15:12:43 -0400

    Add some functions to retrieve specific information from the data

--- src/data/NHLDataManager.py
+++ src/data/NHLDataManager.py
@@ -20,7 +20,7 @@ class NHLDataManager:
 
         if 'NHL_DATA_DIR' in os.environ:
             env = os.environ['NHL_DATA_DIR']
-            print(f'This is your NHL_DATA_DIR environment: {env}')
+            #print(f'This is your NHL_DATA_DIR environment: {env}')
             pass
         else:
             env = input('Enter NHL_DATA_DIR environment: ')
@@ -131,6 +131,45 @@ class NHLDataManager:
             return f'{season_year}03{str(game_number).zfill(4)}'
 
 
+    def download_game(self, season_year: list, is_regular: bool, game_number: int, path_output="") -> dict:
+
+        if path_output == "":
+            if "NHL_DATA_DIR" in os.environ:
+                path_output = os.environ["NHL_DATA_DIR"]
+            else:
+                print('NHL_DATA_DIR environment is not defined, please defined it before to continue.')
+                return None
+
+        if is_regular:
+            path_output_season = os.path.join(path_output, str(season_year), "regular")
+        else:
+            path_output_season = os.path.join(path_output, str(season_year), "playoffs")
+
+        os.makedirs(path_output_season, exist_ok=True)
+
+        game_id = self.build_game_id(season_year, is_regular, game_number)
+        game_id_path = os.path.join(path_output_season, f'{game_id}.json')
+
+        # If the json has already been download, just read it and go the next one
+        if os.path.exists(game_id_path):
+            try:
+                json_dict = json.load(open(game_id_path))
+                return json_dict
+            except json.JSONDecodeError: # if the json file is not valid, retrieve it from the API
+                pass
+
+        # If not, download and save the json
+        url = self.get_url(f'{game_id}')
+        r = requests.get(url)
+        if r.status_code == 200:
+            data_json = r.json()
+            json.dump(data_json, open(game_id_path, "w"), indent=4)
+            return data_json
+        else:
+            return None
+
+
+
     def download_data(self, seasons_year: list, is_regular: bool, path_output="") -> dict:
         """Download data of season "season_year/season_year+1" and put them in the directory specified by path_output.
            By default, this directory is be specified by the environment variable NHL_DATA_DIR.
@@ -142,7 +181,7 @@ class NHLDataManager:
         :type is_regular: bool
         :param path_output: specific game number
         :type path_output: str
-        :return: a dictionary that contains the data downloaded
+        :return: a dictionary that contains the data downloaded. The keys are the season year
         :rtype: dict
         """
 
@@ -184,8 +223,8 @@ class NHLDataManager:
                 # If the json has already been download, just read it and go the next one
                 if os.path.exists(game_id_path):
                     try:
-                        json_file = json.load(open(game_id_path))
-                        self.nhl_data[season_year].append(json_file)
+                        json_dict = json.load(open(game_id_path))
+                        self.nhl_data[season_year].append(json_dict)
                         continue
                     except json.JSONDecodeError: # if the json file is not valid, retrieve it from the API
                         pass
@@ -223,3 +262,148 @@ class NHLDataManager:
                         self.nhl_data[keys].append(data)
 
         return self
+
+
+
+def get_game_ids(season_year:int, season_type:str, path_data="") -> list:
+    """Return the list of game_id for a specific season year and type (regular or playoffs)
+
+    :param season_year: specific season year
+    :type season_year: int
+    :param season_type: 'Regular' or 'Playoffs'
+    :type season_type: str
+    :param path_data: path where the data will be [read if exist] - [stored if not]
+    :type path_data: str
+    :return: the list of game_id (8 digits)
+    :rtype: list[int]
+    """
+
+    if path_data=="":
+        path_data = os.environ["NHL_DATA_DIR"]
+
+    if season_type.lower() == "regular":
+        path_data = os.path.join(path_data, str(season_year), "Regular")
+    elif (season_type.lower() == "playoff") | (season_type.lower() == "playoffs"):
+        path_data = os.path.join(path_data, str(season_year), "playoffs")
+
+    if not os.path.exists(path_data):
+        data_manager = NHLDataManager(path_data)
+        data_manager.download_data(seasons_year=[season_year], is_regular=(season_type.lower() == "regular"), path_output=path_data)
+
+    json_files = os.listdir(path_data)
+    json_files = [f for f in json_files if f.endswith('.json')]
+    
+    games_id = [int(f[0:8]) for f in json_files]
+
+    return games_id
+
+
+def get_game_numbers(season_year:int, season_type:str, path_data="") -> list:
+    """Return the list of game number for a specific season year and type (regular or playoffs)
+
+    :param season_year: specific season year
+    :type season_year: int
+    :param season_type: 'Regular' or 'Playoffs'
+    :type season_type: str
+    :param path_data: path where the data will be [read if exist] - [stored if not]
+    :type path_data: str
+    :return: the list of game number (3 or 4 digits)
+    :rtype: list[int]
+    """
+
+    if path_data=="":
+        path_data = os.environ["NHL_DATA_DIR"]
+
+    if season_type.lower() == "regular":
+        path_data = os.path.join(path_data, str(season_year), "Regular")
+    elif (season_type.lower() == "playoff") | (season_type.lower() == "playoffs"):
+        path_data = os.path.join(path_data, str(season_year), "playoffs")
+
+    if not os.path.exists(path_data):
+        data_manager = NHLDataManager(path_data)
+        data_manager.download_data(seasons_year=[season_year], is_regular=(season_type.lower() == "regular"), path_output=path_data)
+
+    json_files = os.listdir(path_data)
+    json_files = [f for f in json_files if f.endswith('.json')]
+    
+    games_number = [int(f[6:10]) for f in json_files]
+
+    return games_number
+
+
+
+def get_teams(season_year:int, season_type:str, game_number:int, path_data="") -> dict:
+
+    """Return the teams of a specific game
+
+    :param season_year: specific season year
+    :type season_year: int
+    :param season_type: 'Regular' or 'Playoffs'
+    :type season_type: str
+    :param game_num: specific game number (could be get from the get_game_numbers() function)
+    :type game_num: int
+    :param path_data: path where the data will be [read if exist] - [stored if not]
+    :type path_data: str
+    :return: a dictionary {abbr_home:name_home, abbr_away:name_away}
+    :rtype: dict
+    """
+
+    data_manager = NHLDataManager()
+    data = data_manager.download_game(season_year=season_year, is_regular=(season_type.lower() == "regular"), game_number=game_number, path_output=path_data)
+
+    if data is None:
+        return {}
+
+    try:
+        team_name_away = data['gameData']['teams']['away']['name']
+        team_abbr_away = data['gameData']['teams']['away']['abbreviation']
+
+        team_name_home = data['gameData']['teams']['home']['name']
+        team_abbr_home = data['gameData']['teams']['home']['abbreviation']
+
+        return {team_abbr_home:team_name_home, team_abbr_away:team_name_away}
+    except KeyError:
+        return {}
+
+
+def get_final_score(season_year:int, season_type:str, game_number:int, path_data="") -> dict:
+    """Return the final score of a specific game
+
+    :param season_year: specific season year
+    :type season_year: int
+    :param season_type: 'Regular' or 'Playoffs'
+    :type season_type: str
+    :param game_num: specific game number (could be get from the get_game_numbers() function)
+    :type game_num: int
+    :param path_data: path where the data will be [read if exist] - [stored if not]
+    :type path_data: str
+    :return: a dictionary {abbr_home:score, abbr_away:score}
+    :rtype: dict
+    """
+
+    data_manager = NHLDataManager()
+    data = data_manager.download_game(season_year=season_year, is_regular=(season_type.lower() == "regular"), game_number=game_number, path_output=path_data)
+
+    if data is None:
+        return {}
+
+    try:
+        team_abbr_away = data['gameData']['teams']['away']['abbreviation']
+        team_abbr_home = data['gameData']['teams']['home']['abbreviation']
+
+    except KeyError:
+        return {}
+
+
+    try:
+        score_away = data['liveData']['boxscore']['teams']['away']['teamStats']['teamSkaterStats']['goals']
+        score_home = data['liveData']['boxscore']['teams']['home']['teamStats']['teamSkaterStats']['goals']
+
+        
+    except KeyError:
+        score_away = 0
+        score_home = 0
+
+    return {team_abbr_home:score_home, team_abbr_away:score_away}
+
+
