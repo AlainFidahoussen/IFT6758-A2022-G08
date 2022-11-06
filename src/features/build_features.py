9e210b4e5728d2741a672dce7cd62f180d906c60
An Ni Wu <anni.wu28@gmail.com>
2022-11-06 18:29:47 -0500

    Changed histogram to side by side and minor bin modification

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -25,18 +25,19 @@ def build_features(seasons_year):
 
 
         features_data_df.dropna(subset=['st_X', 'st_Y'], inplace=True)
+        features_data_df.reset_index(drop=True, inplace=True)
 
         net_coordinates = np.array([89, 0])
         p2 = np.array([0, 0])
 
-        # features_data_df['Shot distance'] = features_data_df.apply(lambda row: np.linalg.norm(row[['st_X', 'st_Y']]-net_coordinates), axis=1)
-        features_data_df['Shot distance'] = np.linalg.norm(features_data_df[['st_X', 'st_Y']].to_numpy() - net_coordinates, axis=1) # Goal is located at (89, 0)
-        features_data_df['Shot angle'] = features_data_df.apply(lambda row: calculate_angle(row[['st_X', 'st_Y']].to_numpy(), net_coordinates, p2), axis=1)
+        # features_data_df['Shot Distance'] = np.linalg.norm(np.array([features_data_df['st_X'], features_data_df['st_Y']]) - net_coordinates, axis=1) # Goal is located at (89, 0)
+        features_data_df['Shot distance'] = features_data_df.apply(lambda row: np.linalg.norm(np.array([row['st_X'], row['st_Y']]) - net_coordinates), axis=1)
+        features_data_df['Shot angle'] = features_data_df.apply(lambda row: calculate_angle(np.array([row['st_X'], row['st_Y']]), net_coordinates, p2), axis=1)
         features_data_df['Is Goal'] = features_data_df.apply(lambda row: 1 if row['Type'] == 'GOAL' else 0, axis=1)
-        features_data_df.drop(['Type'], axis=1, inplace=True)
+        # features_data_df.drop(['Type'], axis=1, inplace=True) # I need this column for pivot_table
 
         features_data_df['Is Empty'] = features_data_df.apply(lambda row: 1 if row['Empty Net'] == True else 0, axis=1)
-        features_data_df.drop(['Empty Net'], axis=1, inplace=True)
+        features_data_df.drop(['Empty Net'], axis=1, inplace=True) 
 
         features_data_df['Game seconds'] = features_data_df.apply(lambda row: int(row['Time'].split(':')[0])*60 + int(row['Time'].split(':')[1]), axis=1)
         features_data_df.drop(['Time'], axis=1, inplace=True)
@@ -46,19 +47,13 @@ def build_features(seasons_year):
     return features_data_df
 
 
-# Q1 - Relative angle
 def calculate_angle(a, b, c):
     
     try:
-        a = np.array(a)
-        b = np.array(b)
-        c = np.array(c)
+        v0 = a - b
+        v1 = c - b
 
-        ba = a - b
-        bc = c - b
-
-        cosine_angle = np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc))
-        angle = np.degrees(np.arccos(cosine_angle))
+        angle = np.degrees(np.math.atan2(np.linalg.det([v0,v1]),np.dot(v0,v1)))
         
     except:
         angle = np.nan
