58b976e628cd5bd4ffef19112b630f714a8a3a56
An Ni Wu <anni.wu28@gmail.com>
2022-11-19 20:16:48 -0500

    Implemented PCA class

--- src/features/select_features.py
+++ src/features/select_features.py
@@ -6,13 +6,13 @@ RANDOM_SEED = 42
 import os
 
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
+
 import pickle
 
 
-from sklearn.model_selection import StratifiedKFold
-from sklearn.feature_selection import RFECV
+from imblearn.ensemble import BalancedRandomForestClassifier
 from sklearn.feature_selection import SelectFromModel
+from sklearn.feature_selection import RFECV
 
 from sklearn.base import BaseEstimator, TransformerMixin
 
@@ -21,11 +21,12 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 from sklearn.svm import LinearSVC
-from sklearn.feature_selection import SelectFromModel
 from sklearn.decomposition import PCA
+
 from sklearn.preprocessing import StandardScaler
 
-class SelectFromRandomForest(BaseEstimator, TransformerMixin):
+
+class SelectFromTree(BaseEstimator, TransformerMixin):
 
     def __init__(self, **kwargs):
         dir = os.path.join(os.environ['NHL_MODEL_DIR'], 'FeaturesSelector')
@@ -65,6 +66,49 @@ class SelectFromRandomForest(BaseEstimator, TransformerMixin):
             return X, y
 
 
+
+class SelectFromTree_RecursiveElimination(BaseEstimator, TransformerMixin):
+
+    def init(self, **kwargs):
+        dir = os.path.join(os.environ['NHL_MODEL_DIR'], 'FeaturesSelector')
+        self.pkl_dir = dir
+
+    def fit(self, X, y=None):
+
+        filename = os.path.join(self.pkl_dir, 'tree_recursive_elimination.pkl')
+        if os.path.exists(filename):
+            with open(filename, 'rb') as file:
+                selector = pickle.load(file)
+            return selector
+
+        else:
+            min_features_to_select = 1
+
+            selector = RFECV(
+                estimator=DecisionTreeClassifier(),
+                step=1,
+                cv=StratifiedKFold(2),
+                scoring="f1_macro",
+                min_features_to_select=min_features_to_select,
+            )
+            selector.fit(X, y)
+
+            os.makedirs(self.pkl_dir, exist_ok=True)
+            with open(os.path.join(self.pkl_dir, 'tree_recursive_elimination.pkl'), 'wb') as file:
+                pickle.dump(selector, file)
+
+
+    def transform(self, X, y=None):
+
+        filename = os.path.join(self.pkl_dir, 'tree_recursive_elimination.pkl')
+        if os.path.exists(filename):
+            with open(filename, 'rb') as file:
+                selector = pickle.load(file)
+            return selector.transform(X), y
+        else:
+            return X, y
+        
+        
 class SelectFromLinearSVC(BaseEstimator, TransformerMixin):
 
     def __init__(self, **kwargs):
@@ -78,8 +122,8 @@ class SelectFromLinearSVC(BaseEstimator, TransformerMixin):
         if os.path.exists(filename):
             with open(filename, 'rb') as file:
                 selector = pickle.load(file)
-            feature_names = np.array(X.columns)
-            self.selected_features = feature_names[selector.get_support()]
+            # feature_names = np.array(X.columns)
+            # self.selected_features = feature_names[selector.get_support()]
             
             return selector
 
@@ -89,9 +133,9 @@ class SelectFromLinearSVC(BaseEstimator, TransformerMixin):
                 penalty="l1", 
                 dual=False,
                 random_state=RANDOM_SEED))
-            selector.fit(X, y)
-            feature_names = np.array(X.columns)
-            self.selected_features = feature_names[selector.get_support()]
+            # selector.fit(X, y)
+            # feature_names = np.array(X.columns)
+            # self.selected_features = feature_names[selector.get_support()]
 
             os.makedirs(self.pkl_dir, exist_ok=True)
             with open(os.path.join(self.pkl_dir, 'linearSVC.pkl'), 'wb') as file:
@@ -107,39 +151,100 @@ class SelectFromLinearSVC(BaseEstimator, TransformerMixin):
             with open(filename, 'rb') as file:
                 selector = pickle.load(file)
         
-            return pd.DataFrame(selector.transform(X), columns=self.selected_features), y
+            # return pd.DataFrame(selector.transform(X), columns=self.selected_features), y
+            return selector.transform(X), y
         else:
             return X, y
 
-
-def SelectFromPCA(X, y, n_components):
-    continuous_col = ['Period seconds', 'st_X', 'st_Y', 'Shot distance',
-       'Shot angle', 'Speed From Previous Event',
-       'Change in Shot Angle', 'Shooter Goal Ratio Last Season',
-       'Goalie Goal Ratio Last Season', 'Elapsed time since Power Play',
-       'Last event elapsed time', 'Last event st_X', 'Last event st_Y',
-       'Last event distance', 'Last event angle']
-    
-    X_cont, X_other = X[continuous_col], X.drop(continuous_col, axis=1) # 15, 22
-    X_cont_st = StandardScaler().fit_transform(X_cont)
-    pca = PCA(n_components=n_components)
-    X_PCA = pca.fit_transform(X_cont_st)
-    cev = np.cumsum(pca.explained_variance_ratio_)
-    cev = np.insert(cev, 0, 0) # add 0 if 0 components
-    
-    principalDf = pd.DataFrame(data = X_PCA, columns = [f'PC{i}' for i in range(1, n_components+1)])
-    X_final = pd.concat([principalDf, X_other], axis = 1)
-    
-    print(f'Cumulative explained variance with {n_components} components: {cev[-1]}')
         
-    plt.figure(figsize=(15,10))
-    plt.ylim(0.0,1.1)
-    plt.plot(cev, linewidth=3)
-    plt.xlabel('number of components', fontsize=21)
-    plt.ylabel('cumulative explained variance', fontsize=21)
-    plt.title('Scree Plot using PCA', fontsize=24)
-    plt.rc('font', size=16)
-    plt.grid()
-    plt.show()
+class SelectFromPCA():
+
+    def __init__(self, n_components):
+        self.n_components = n_components
+        self.selector = None
+        
     
-    return X_final, y
+    def separate_X(self, X) :
+        continuous_col = ['Period seconds', 'st_X', 'st_Y', 'Shot distance',
+           'Shot angle', 'Speed From Previous Event',
+           'Change in Shot Angle', 'Shooter Goal Ratio Last Season',
+           'Goalie Goal Ratio Last Season', 'Elapsed time since Power Play',
+           'Last event elapsed time', 'Last event st_X', 'Last event st_Y',
+           'Last event distance', 'Last event angle']
+
+        X_cont, X_other = X[continuous_col], X.drop(continuous_col, axis=1)
+        
+        
+        return X_cont, X_other
+        
+    def fit(self, X, y=None):
+        
+        X_cont, X_other = self.separate_X(X)
+        
+        X_cont_st = StandardScaler().fit_transform(X_cont)
+
+        self.selector = PCA(n_components=self.n_components)
+        self.selector.fit(X_cont_st)  
+
+        return self.selector
+
+
+    def transform(self, X, y=None):
+        if self.selector is not None: 
+            
+            X_cont, X_other = self.separate_X(X)
+            X_PCA = self.selector.transform(X_cont)
+            
+            principalDf = pd.DataFrame(data = X_PCA, columns = [f'PC{i}' for i in range(1, self.n_components+1)])
+            X_final = pd.concat([principalDf, X_other], axis = 1)
+            
+            cev = np.cumsum(self.selector.explained_variance_ratio_)
+            cev = np.insert(cev, 0, 0)
+            print(f'Cumulative explained variance with {self.n_components} components: {cev[-1]}')
+
+            plt.figure(figsize=(15,10))
+            plt.ylim(0.0,1.1)
+            plt.plot(cev, linewidth=3)
+            plt.xlabel('number of components', fontsize=21)
+            plt.ylabel('cumulative explained variance', fontsize=21)
+            plt.title('Scree Plot using PCA', fontsize=24)
+            plt.rc('font', size=16)
+            plt.grid()
+            plt.show()
+        
+            return X_final, y
+        else:
+            return X, y
+
+        
+# def SelectFromPCA(X, y, n_components):
+#     continuous_col = ['Period seconds', 'st_X', 'st_Y', 'Shot distance',
+#        'Shot angle', 'Speed From Previous Event',
+#        'Change in Shot Angle', 'Shooter Goal Ratio Last Season',
+#        'Goalie Goal Ratio Last Season', 'Elapsed time since Power Play',
+#        'Last event elapsed time', 'Last event st_X', 'Last event st_Y',
+#        'Last event distance', 'Last event angle']
+    
+#     X_cont, X_other = X[continuous_col], X.drop(continuous_col, axis=1) # 15, 22
+#     X_cont_st = StandardScaler().fit_transform(X_cont)
+#     pca = PCA(n_components=n_components)
+#     X_PCA = pca.fit_transform(X_cont_st)
+#     cev = np.cumsum(pca.explained_variance_ratio_)
+#     cev = np.insert(cev, 0, 0) # add 0 if 0 components
+    
+#     principalDf = pd.DataFrame(data = X_PCA, columns = [f'PC{i}' for i in range(1, n_components+1)])
+#     X_final = pd.concat([principalDf, X_other], axis = 1)
+    
+#     print(f'Cumulative explained variance with {n_components} components: {cev[-1]}')
+        
+#     plt.figure(figsize=(15,10))
+#     plt.ylim(0.0,1.1)
+#     plt.plot(cev, linewidth=3)
+#     plt.xlabel('number of components', fontsize=21)
+#     plt.ylabel('cumulative explained variance', fontsize=21)
+#     plt.title('Scree Plot using PCA', fontsize=24)
+#     plt.rc('font', size=16)
+#     plt.grid()
+#     plt.show()
+    
+#     return X_final, y
